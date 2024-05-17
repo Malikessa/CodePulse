6db641a5dd797d64ee928ffe83697b480104a028
ghulam.essa <ghulam.essa@curemd.com>
2024-05-17 09:38:44 -0400

    Tables Ceation in DB using EF Core, Controller Creation and Create Action Method

---
+++ API/CodePulse/CodePulse/Controllers/CategoryController.cs
@@ -0,0 +1,44 @@
+﻿using CodePulse.Data;
+using CodePulse.Models.Domain;
+using CodePulse.Models.DTO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodePulse.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly ApplicationDbContext dBContext;
+        public CategoryController(ApplicationDbContext dbContext)
+        {
+            this.dBContext = dbContext;
+        }
+
+        //
+        [HttpPost]
+        public async Task<IActionResult> CreateCategory(CreateCategoryRequestDto request)
+        {
+            // Map DTO to Domain Model
+            var category = new Category
+            {
+                Name = request.Name,
+                UrlHandle=request.UrlHandle
+            };
+
+            await dBContext.Categories.AddAsync(category);
+            await dBContext.SaveChangesAsync();
+
+            var response = new CategoryDto
+            {
+                Id = category.Id,
+                Name = category.Name,
+                UrlHandle = category.UrlHandle
+            };
+
+            return Ok(response);
+
+        }
+    }
+}
