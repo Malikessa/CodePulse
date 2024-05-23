289d046ad944dbed18954f629347fde9bb6002f0
ghulam.essa <ghulam.essa@curemd.com>
2024-05-23 03:10:02 -0400

    Removing UI of Angular 17

--- UI/codepulse/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { CategoryListComponent } from './features/category/category-list/category-list.component';
-
-export const routes: Routes = [
-    {
-        path:'admin/categories',
-        component:CategoryListComponent
-    }
-];
